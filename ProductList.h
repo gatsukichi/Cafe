646cb2ed641885c140466b5a652ddf541f4524a0
limkangho <49905142+limkangho@users.noreply.github.com>
2019-08-21 21:35:51 +0900

    Update ProductList.h

--- ProductList.h
+++ ProductList.h
@@ -16,25 +16,60 @@ private:
     StrawberrySmoothie strawberrysmoothie;
     ChocoSmoothie chocosmoothie;
 public:
-    int getAmericanoPrice() const;
-    int getCaffeLattePrice() const;
-    int getCaffeMochaPrice() const;
-    int getBlackTeaPrice() const;
-    int getGreenTeaPrice() const;
-    int getStrawberryAdePrice() const;
-    int getLemonadePrice() const;
-    int getStrawberrysmoothiePrice() const;
-    int getChocosmoothiePrice() const;
+    int Product::getAmericanoPrice() const{
+        return this->americano.getPrice();
+    }
+    int Product::getCaffeLattePrice() const{
+        return this->cafelatte.getPrice();
+    }   
+    int Product::getCaffeMochaPrice() const{
+        return this->cafemocha.getPrice();
+    }
+    int Product::getBlackTeaPrice() const{
+        return this->blacktea.getPrice();
+    }
+    int Product::getGreenTeaPrice() const{
+        return this->greentea.getPrice();
+    }
+    int Product::getStrawberryAdePrice() const{
+        return this->strawberryade.getPrice();
+    }
+    int Product::getLemonadePrice() const{
+        return this->lemonade.getPrice();
+    }
+    int Product::getStrawberrysmoothiePrice() const{
+        return this->strawberrysmoothie.getPrice();
+    }
+    int Product::getChocosmoothiePrice() const{
+        return this->chocosmoothie.getPrice();
+    }
     
-//    void setAmericanoPrice(int price);
-//    void setCaffeLattePrice(int price);
-//    void setCafeMochaPrice(int price);
-//    void setBlackTeaPrice(int price);
-//    void setGreenTeaPrice(int price);
-//    void setStrawberryAdePrice(int price);
-//    void setLemonAdePrice(int price);
-//    void setStrawberrySmoothiePrice(int price);
-//    void setChocoSmoothiePrice(int price);
+//     void setAmericanoPrice(int price) {
+//         this->price = price;
+//     }
+//     void setCaffeLattePrice(int price) {
+//         this->price = price;
+//     }
+//     void setCafeMochaPrice(int price) {
+//         this->price = price;
+//     }
+//     void setBlackTeaPrice(int price) {
+//         this->price = price;
+//     }
+//     void setGreenTeaPrice(int price) {
+//         this->price = price;
+//     }
+//     void setStrawberryAdePrice(int price) {
+//         this->price = price;
+//     }
+//     void setLemonAdePrice(int price) {
+//         this->price = price;
+//     }
+//     void setStrawberrySmoothiePrice(int price) {
+//         this->price = price;
+//     }
+//     void setChocoSmoothiePrice(int price) {
+//         this->price = price;
+//     }
 
-    
 };
