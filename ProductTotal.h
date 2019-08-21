730a6ce266b8060629349baccab9a4527d1b508e
limkangho <49905142+limkangho@users.noreply.github.com>
2019-08-21 21:43:18 +0900

    Update ProductTotal.h

--- ProductTotal.h
+++ ProductTotal.h
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
-
 #include "Product.h"
-
 #define DRINK_MAX_CNT 9
 
 class ProductTotal{
 private:
-    
     int DrinkTotList[DRINK_MAX_CNT];
-    
     
     //int totAme;
     //int totCafeLatte;
@@ -21,22 +17,20 @@ private:
     //int totStrawberrySmoothie;
     //int totChocoSmoothie;
     
-    
 public:
-    void setDrinkTotList(int cnt, int index){ // 팔려서 누적할 cnt, 접근할 방 번호 상수인 index
+    void setDrinkTotList(int cnt, int index) { // 팔려서 누적할 cnt, 접근할 방 번호 상수인 index
         this->DrinkTotList[index] += cnt;
     }
-    int * getDrinkTot(){
+    int * getDrinkTot() {
         return this->DrinkTotList;
     }
-    int getDrinkTot(int index){
+    int getDrinkTot(int index) {
         return this->DrinkTotList[index];
     }
-
-    void totalView(){
+    void totalView() {
         cout << "- Drink Total -" << endl;
-        for (int i = 0; i<DRINK_MAX_CNT; i++) {
-            cout << i << ". " << this->DrinkTotList[i] << endl;
+        for (int i=0; i<DRINK_MAX_CNT; i++) {
+            cout << i+1 << ". " << this->DrinkTotList[i] << endl;
         }
     }
 };
