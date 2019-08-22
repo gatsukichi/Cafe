e22a11ac953418a3f5a6c92f423aeeb0e6ebfd9f
limkangho <49905142+limkangho@users.noreply.github.com>
2019-08-22 16:47:32 +0900

    Update IngredientList.h

--- IngredientList.h
+++ IngredientList.h
@@ -1,3 +1,6 @@
+#ifndef INGREDIENTLIST_H_
+#define INGREDIENTLIST_H_
+
 #include <iostream>
 using namespace std;
 #include"Ingredient.h"
@@ -188,3 +191,5 @@ public:
         sugar.increaseSugarGram(gram);
     }
 };
+
+#endif /* INGREDIENTLIST_H_ */
