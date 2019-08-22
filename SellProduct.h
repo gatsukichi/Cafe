3066cc94d6b923fa320d54da946c100278479222
chae-chae <54352086+chae-chae@users.noreply.github.com>
2019-08-22 19:49:54 +0900

    Update SellProduct.h

--- SellProduct.h
+++ SellProduct.h
@@ -4,40 +4,41 @@
 #include "IngredientList.h"
 #include "MoneyBox.h"
 #include "ProductList.h"
-#define DE_COFFEEBEAN 100
-#define DE_MILK 100
-#define DE_CHOCO 100
-#define DE_BLACKTEABAG 100
-#define DE_GREENTEABAG 100
-#define DE_CIDER 100
-#define DE_STRAWBERRY 100
-#define DE_LEMON 100
-#define DE_EGG 100
+
+#define DE_COFFEEBEAN 20
+#define DE_MILK 50
+#define DE_CHOCO 30
+#define DE_BLACKTEABAG 30
+#define DE_GREENTEABAG 30
+#define DE_CIDER 50
+#define DE_STRAWBERRY 50
+#define DE_LEMON 50
+#define DE_EGG 1
 #define DE_FLOUR 100
-#define DE_SUGAR 100
+#define DE_SUGAR 20
 
 
 class SellProduct {
 public:
-	int ProductCntIncreament(ProductList &PL, int index, int count) {
-		int sellM;
-		switch (index) {
-		case 1: sellM = PL.getAmericanoPrice() * count; break;
-		case 2: sellM = PL.getCaffeLattePrice() * count; break;
-		case 3: sellM = PL.getCaffeMochaPrice() * count; break;
-		case 4: sellM = PL.getGreenTeaPrice() * count; break;
-		case 5: sellM = PL.getBlackTeaPrice() * count; break;
-		case 6: sellM = PL.getStrawberryAdePrice() * count; break;
-		case 7: sellM = PL.getLemonadePrice() * count; break;
-		case 8: sellM = PL.getStrawberrysmoothiePrice() * count; break;
-		case 9: sellM = PL.getChocosmoothiePrice() * count; break;
-        default: sellM=0; break;
-		}
-
-		return sellM;
-	}
-
-	void IngredientCntDecreament(IngredientList& IL, int index, int count) { //재료리스트 (재료가 빠져야하니까), 카운트는 몇잔샀는지에 따라서 빠져야하니까, 인덱스는 뭘 샀는지 알아야하니까
+    int ProductCntIncreament(ProductList &PL, int index, int count) {
+        int sellM;
+        switch (index) {
+            case 1: sellM = PL.getAmericanoPrice() * count; break;
+            case 2: sellM = PL.getCaffeLattePrice() * count; break;
+            case 3: sellM = PL.getCaffeMochaPrice() * count; break;
+            case 4: sellM = PL.getGreenTeaPrice() * count; break;
+            case 5: sellM = PL.getBlackTeaPrice() * count; break;
+            case 6: sellM = PL.getStrawberryAdePrice() * count; break;
+            case 7: sellM = PL.getLemonadePrice() * count; break;
+            case 8: sellM = PL.getStrawberrysmoothiePrice() * count; break;
+            case 9: sellM = PL.getChocosmoothiePrice() * count; break;
+            default: sellM=0; break;
+        }
+        
+        return sellM;
+    }
+    
+    void IngredientCntDecreament(IngredientList& IL, int index, int count) { //재료리스트 (재료가 빠져야하니까), 카운트는 몇잔샀는지에 따라서 빠져야하니까, 인덱스는 뭘 샀는지 알아야하니까
         switch (index) {
             case 1: // 아메리카노
                 IL.decreaseCoffeeBeanGram(DE_COFFEEBEAN*count);
@@ -76,11 +77,11 @@ public:
                 break;
         }
     }
-
-	void MoneyIncreament(MoneyBox &MB, int sellM) {
-		MB.accumulateSellM(sellM);
-	}
-	
+    
+    void MoneyIncreament(MoneyBox &MB, int sellM) {
+        MB.accumulateSellM(sellM);
+    }
+    
 };
 
 #endif /* SELLPRODUCT_H_ */
